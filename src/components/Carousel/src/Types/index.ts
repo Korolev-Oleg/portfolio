272f717f537392e65fb18d549fbd1c5782350635
1koro <ok@hustn.cn>
2022-08-12 20:14:17 +0300

    update

---
+++ src/components/Carousel/src/Types/index.ts
@@ -0,0 +1,7 @@
+export type CarouselModel = {
+	githubLink?: string | null;
+	previewLink?: string | null;
+	headerText?: string | null;
+	subText?: string | null;
+	image: string;
+};
---
+++ src/components/SectionWeb/SectionWeb.js
@@ -0,0 +1,56 @@
+import {Carousel} from '../Carousel/src'
+import {FaArrowLeft, FaArrowRight} from 'react-icons/fa'
+import './web.css'
+import prodlogistica from './video/prodlogistica.mp4'
+import fruitprod from './video/fruitprod.mp4'
+import cleaning from './video/cleaning.mp4'
+import steamsales from './video/steamsales.mp4'
+
+
+// import steamsales from './images/steamsales.gif'
+
+
+const WebCarousel = () => {
+
+  const CarouselData = [
+    {
+      githubLink: 'https://github.com/Korolev-Oleg/Korolev-Oleg.github.io/tree/master/steamsales',
+      previewLink: '/steamsales',
+      image: steamsales,
+    },
+    {
+      githubLink: 'https://github.com/Korolev-Oleg/Korolev-Oleg.github.io/tree/master/prodlogistica',
+      previewLink: 'https://prodlogistica.ru/',
+      image: prodlogistica,
+    },
+    {
+      githubLink: 'https://github.com/Korolev-Oleg/Korolev-Oleg.github.io/tree/master/cleaning',
+      previewLink: '/cleaning',
+      image: cleaning,
+    },
+    {
+      githubLink: 'https://github.com/Korolev-Oleg/Korolev-Oleg.github.io/tree/master/fruit',
+      previewLink: '/fruit',
+      image: fruitprod,
+    },
+  ]
+  return (
+    <div className="section-web">
+      <h2>Web development</h2>
+      <div className='web-carousel'>
+        <Carousel
+          data={CarouselData}
+          autoPlay={true}
+          rightItem={<FaArrowRight/>}
+          leftItem={<FaArrowLeft/>}
+          animationDuration={3000}
+          headerTextType="black"
+          subTextType="white"
+          size="normal"
+        />
+      </div>
+    </div>
+  )
+}
+
+export default WebCarousel
